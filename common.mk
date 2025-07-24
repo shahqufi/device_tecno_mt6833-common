22bd732f58e2473302a0d5a95b54bac078ff6e9f
shahqufi <shahqufi@gmail.com>
2025-07-24 23:22:41 +0530

    Add unpinned blobs for Tecno Dolby support in proprietary-fles.txt

--- common.mk
+++ common.mk
@@ -13,11 +13,38 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/vabc_features.m
 # A/B
 AB_OTA_UPDATER := true
 AB_OTA_PARTITIONS += \
+    boot \
     system \
     product \
     system_ext \
     vendor \
+	vendor_boot \
+	vbmeta \
     vbmeta_system \
-    vbmeta_vendor \
-    boot
+    vbmeta_vendor 
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=$(BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE) \
+    POSTINSTALL_OPTIONAL_system=true
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_vendor=true \
+    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+    FILESYSTEM_TYPE_vendor=$(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE) \
+    POSTINSTALL_OPTIONAL_vendor=true
+
+PRODUCT_PACKAGES += \
+    update_engine \
+    update_engine_sideload \
+    update_verifier \
+    otapreopt_script \
+    checkpoint_gc
+
+PRODUCT_PACKAGES += \
+    create_pl_dev \
+    create_pl_dev.recovery
+
+PRODUCT_VIRTUAL_AB_COMPRESSION_METHOD := lz4
 
